fb91a367cbc545672b33176ce64c347d0122318d
Arceus3251 <arceus3251@gmail.com>
2020-11-02 13:57:36 -0600

    Learning How to make custom Items
    Suddenly the peaceful music is turning into heavy metal. Oh god Oh Fuck Help

---
+++ src/main/java/com/DPN/ProjectS/init/ModItems.java
@@ -0,0 +1,12 @@
+package com.DPN.ProjectS.init;
+
+import com.DPN.ProjectS.items.ItemBase;
+import net.minecraft.item.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModItems {
+    public static final List<Item> ITEMS = new ArrayList<Item>();
+    public static final Item TEST_ITEM = new ItemBase("test_item");
+}
---
+++ src/main/java/com/DPN/ProjectS/util/IHasModel.java
@@ -0,0 +1,5 @@
+package com.DPN.ProjectS.util;
+
+public interface IHasModel {
+    public void registerModels();
+}
