8bdca22f80948eadad65b735c7fac6347a0f35cc
Arceus3251 <arceus3251@gmail.com>
2020-11-02 12:20:07 -0600

    Defining Info about the mod
    I'm gonna run all the colors across the bottom of the screen that you'll need to paint along with me today.

---
+++ src/main/java/com/DPN/ProjectS/Main.java
@@ -0,0 +1,8 @@
+package com.DPN.ProjectS;
+
+import com.DPN.ProjectS.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid= Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+}
---
+++ src/main/java/com/DPN/ProjectS/util/Reference.java
@@ -0,0 +1,10 @@
+package com.DPN.ProjectS.util;
+
+public class Reference {
+    public static final String MOD_ID = "ps";
+    public static final String NAME = "ProjectS";
+    public static final String VERSION = "0.1a";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String CLIENT_PROXY_CLASS = "";
+    public static final String COMMON_PROXY_CLASS = "";
+}
